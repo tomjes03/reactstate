bd02cce97a62da6c35d21f2a2706062982b73bb2
Emmabata <ndinezeemmanuel@gmail.com>
2023-04-03 18:31:23 +0100

    latest commit

--- statecheckpoint/src/App.js
+++ statecheckpoint/src/App.js
@@ -3,18 +3,52 @@ import { Component } from 'react'
 
 class App extends Component {
   state = {
-    fullName : "Adam",
-    bio : "will look into it",
-    imgSrc : "logo",
-    profession : "Musician",
     show:"false"
+  
   }
 
+  person = {
+    name : "Adams Klean",
+    profession: "Musician",
+    Bio: "Manuel Iglesias draws from his life story when writing about the experiences of migrant workers. His first series of poems, My Father's Hands, appeared in The New Yorker and describes how his family crossed the Texas border to give Manuel and his brothers a better life. After receiving his Master's in Fine Arts from Columbia University, Manuel wrote three nonfiction novels about his experiences, including Under the Streaming Sun, which earned the National Prize for Arts and Sciences in 2008. Manuel is currently working on a collection of fictional short stories to be published in early 2021."
+
+  }
+  
+ 
 
   render() {
+    var toggle = e => {
+      this.setState({show:!this.state.show})
+    }
+    const checkIt = this.state.show;
+      const styleCard = {
+        boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+        Width: "200px",
+        textAlign: "center",
+        marginTop: "20px",
+        backgroundColor:"#ffe",
+        borderRadius:"20px"
+      }
     return(
       <>
-      <button onClick={this.state.handleClick}>Click me</button>
+      <div style={{marginLeft:"auto", marginRight:"auto", width:"370px", backgroundColor:"darkblue"}}>
+      <div style={styleCard}>
+        <button onClick={toggle}>{checkIt ? "Hide" : "Show"}</button>
+        {
+          checkIt && (<>
+          <center>
+          <img 
+          src= {require("../src/randomprofile.jpg")}
+          alt="random profile" 
+          style={{textAlign:"center", width:"150px"}} />
+          </center>
+          {this.person.name}
+          <h2>{this.person.profession}</h2>
+          <p className='biop'><span className='bio'>Bio</span><br/>{this.person.Bio}</p>
+          </>)
+        }
+      </div>
+      </div>
       </>
     )
   }
---
+++ statecheckpoint/src/Button/Button.jsx
@@ -0,0 +1,19 @@
+// import React, { useState } from 'react';
+
+
+// const Button = (props)=> {
+
+//     const [show, setShow] = useState(true)
+
+//     return(
+//          <>
+//          <button onClick={() => setShow(!show)}>
+//             {show ? "show" : "Hide"}
+//          </button>
+//          {show && {props.person}}
+//          </>
+//     )
+// }
+
+
+// export default Button
