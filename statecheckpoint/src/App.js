096639d81eb6705a3341795be3411a1d9adaa96d
Emmabata <ndinezeemmanuel@gmail.com>
2023-04-05 09:27:31 +0100

    newest commit

--- statecheckpoint/src/App.js
+++ statecheckpoint/src/App.js
@@ -1,39 +1,66 @@
 import './App.css';
+//import component from react in a class component
 import { Component } from 'react'
 
+//write the class component
 class App extends Component {
+  //Declare your state
   state = {
-    show:"false"
-  
+    show:"false",
+    count: 0,
+    date : new Date().toLocaleString(),
   }
 
+  componentDidMount = () => {
+    setInterval(() => {
+      this.setState(
+        prevState => ({
+          count: prevState.count + 1,
+          date : new Date().toLocaleString()
+        })
+      )
+    }, 1000);
+  }
+
+
+ //Declare Person objects
   person = {
     name : "Adams Klean",
     profession: "Musician",
     Bio: "Manuel Iglesias draws from his life story when writing about the experiences of migrant workers. His first series of poems, My Father's Hands, appeared in The New Yorker and describes how his family crossed the Texas border to give Manuel and his brothers a better life. After receiving his Master's in Fine Arts from Columbia University, Manuel wrote three nonfiction novels about his experiences, including Under the Streaming Sun, which earned the National Prize for Arts and Sciences in 2008. Manuel is currently working on a collection of fictional short stories to be published in early 2021."
 
   }
-  
- 
-
+  //rendering the component
   render() {
+    //Modify the state using useState
     var toggle = e => {
       this.setState({show:!this.state.show})
     }
+
+    const {count} = this.state
+    const {date} = this.state
+
+    //Get the updated show and assign it to the checIt variable
     const checkIt = this.state.show;
-      const styleCard = {
+    //return what you want to display on the UI
+    return(
+      //use the empty tag to input more than one jsx and it takes no default styling
+      <>
+      {/* input a the toggle() in the button and assign between the opening and closing button tag    */}
+       <center>
+        <button className='btn' onClick={toggle}>
+        {checkIt ? "click to Hide" : "click to Show"}</button> <span>{count}{date}</span>
+        </center> 
+
+      <div style={{width:"350px",marginLeft:"auto", marginRight:"auto",borderRadius:"10px", backgroundColor:"darkblue"}}>
+      <div style={{
         boxShadow: "0 4px 8px 0 rgba(8, 5, 8, 0.2)",
-        Width: "200px",
+        Width: "350px",
         textAlign: "center",
         marginTop: "20px",
-        backgroundColor:"#ffe",
+        backgroundColor:"#503",
         borderRadius:"20px"
-      }
-    return(
-      <>
-       <center><button onClick={toggle}>{checkIt ? "Hide" : "Show"}</button></center>
-      <div style={{marginLeft:"auto", marginRight:"auto", width:"370px", backgroundColor:"darkblue"}}>
-      <div style={styleCard}>
+      }}>
        
         {
           checkIt && (<>
@@ -44,12 +71,14 @@ class App extends Component {
           style={{textAlign:"center", width:"150px", borderRadius:"10px", marginTop:"10px"}} />
           </center>
           {this.person.name}
-          <h2>{this.person.profession}</h2>
+          <h2 style={{fontFamily:"fantasy", color:"AppWorkspace"}}>{this.person.profession}</h2>
           <p className='biop'><span className='bio'>Bio</span><br/>{this.person.Bio}</p>
           </>)
         }
       </div>
+      
       </div>
+
       </>
     )
   }
